1186e8a125aec952b466390c1afb467aab0a73b9
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-05-19 16:43:26 -0300

    :sparkles: search if the user exists before making a flight booking

--- lib/bookings/create_or_update.ex
+++ lib/bookings/create_or_update.ex
@@ -1,8 +1,11 @@
 defmodule Flightex.Bookings.CreateOrUpdate do
   @moduledoc false
 
-  alias Flightex.Bookings.Agent
+  alias Flightex.Bookings.Agent, as: BookingsAgent
   alias Flightex.Bookings.Booking
+  alias Flightex.Users.Agent, as: UsersAgent
+
+  @all_fields_message "Inform all fields: user_uuid, %{complete_date: value, local_origin: value, local_destination: value}"
 
   @doc """
   Create a flight booking for a user and save it in the agent.
@@ -29,18 +32,33 @@ defmodule Flightex.Bookings.CreateOrUpdate do
         iex> {:ok, booking_id} = Flightex.Bookings.CreateOrUpdate.call(user_uuid, booking_params)
 
   """
-  def call(user_uuid, %{
-        complete_date: complete_date,
-        local_origin: local_origin,
-        local_destination: local_destination
-      }) do
-    complete_date
-    |> Booking.build(local_origin, local_destination, user_uuid)
-    |> save_booking()
+  def call(user_uuid, %{} = booking_params) do
+    with {:ok, _user} <- UsersAgent.get_by_id(user_uuid),
+         {:ok, %Booking{}} = booking <- build_booking(user_uuid, booking_params) do
+      save_booking(booking)
+    else
+      error -> error
+    end
+  end
+
+  def call(_user_uuid, _any) do
+    {:error, @all_fields_message}
+  end
+
+  defp build_booking(user_uuid, %{
+         complete_date: complete_date,
+         local_origin: local_origin,
+         local_destination: local_destination
+       }) do
+    Booking.build(complete_date, local_origin, local_destination, user_uuid)
+  end
+
+  defp build_booking(_user_uuid, _any) do
+    {:error, @all_fields_message}
   end
 
   defp save_booking({:ok, %Booking{id: id} = booking}) do
-    Agent.save(booking)
+    BookingsAgent.save(booking)
 
     {:ok, id}
   end
