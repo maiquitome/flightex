bf8668723da9131216d1bcc430e3936677a4513a
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-05-19 16:37:53 -0300

    :sparkles: get all users, get user by id and get user by cpf

--- lib/users/agent.ex
+++ lib/users/agent.ex
@@ -12,36 +12,105 @@ defmodule Flightex.Users.Agent do
   end
 
   @doc """
-  Returns the user.
+  Returns all registered users.
 
   ## Examples
 
-      iex> user = Flightex.Users.User.build("Maiqui", "maiquitome@gmail.com", "011.123.012-26")
-      %Flightex.Users.User{
-        cpf: "011.123.012-26",
-        email: "maiquitome@gmail.com",
-        id: "3b108193-38fa-48c4-b208-795c8c70b2f5",
-        name: "Maiqui"
-      }
+    - the user agent has to have been initialized
 
-      iex> Flightex.Users.Agent.start_link
-      {:ok, #PID<0.2215.0>}
+          iex> {:ok, _pid} =  Flightex.start_agents
 
-      iex> Flightex.Users.Agent.save(user)
-      {:ok, "3b108193-38fa-48c4-b208-795c8c70b2f5"}
+    - creating a user
 
-      iex> {:ok, user} = Flightex.Users.Agent.get("011.123..012-26")
-      {:ok,
-      %Flightex.Users.User{
-        cpf: "011.123..012-26",
-        email: "maiquitome@gmail.com",
-        id: "3b108193-38fa-48c4-b208-795c8c70b2f5",
-        name: "Maiqui"
-      }}
+          iex> user_params = %{name: "Maiqui", email: "maiquitome@gmail.com", cpf: "011.123.012-26"}
+
+          iex> {:ok, user_uuid} = Flightex.create_user(user_params)
+          {:ok, "b77f161c-13e7-4b61-9bcf-db87587bb179"}
+
+    - searching all users
+
+          iex> Flightex.Users.Agent.get_all
+          %{
+            "011.123.012-26" => %Flightex.Users.User{
+              cpf: "011.123.012-26",
+              email: "maiquitome@gmail.com",
+              id: "36c3a6f9-67b6-4026-a1ef-a18476943e81",
+              name: "Maiqui"
+            }
+          }
 
   """
-  def get(cpf) do
-    Agent.get(__MODULE__, fn state -> get_user(state, cpf) end)
+  def get_all do
+    Agent.get(__MODULE__, & &1)
+  end
+
+  @doc """
+  Get a user searching by id.
+
+  ## Examples
+
+    - the user agent has to have been initialized
+
+          iex> {:ok, _pid} =  Flightex.start_agents
+
+    - creating a user
+
+          iex> user_params = %{name: "Maiqui", email: "maiquitome@gmail.com", cpf: "011.123.012-26"}
+
+          iex> {:ok, user_uuid} = Flightex.create_user(user_params)
+          {:ok, "b77f161c-13e7-4b61-9bcf-db87587bb179"}
+
+    - searching the user by id
+
+          iex> {:ok, user_found} = Flightex.Users.Agent.get_by_id(user_uuid)
+          {:ok,
+          %Flightex.Users.User{
+            cpf: "011.123.012-26",
+            email: "maiquitome@gmail.com",
+            id: "b77f161c-13e7-4b61-9bcf-db87587bb179",
+            name: "Maiqui"
+          }}
+
+    - error: user not found
+
+          iex> Flightex.Users.Agent.get_by_id("801737b0-6c1f-43b7-af51-97de2e574")
+          {:error, "User not found!"}
+
+  """
+  def get_by_id(id) do
+    Agent.get(__MODULE__, fn all_users -> get_user_by_id(all_users, id) end)
+  end
+
+  @doc """
+  Get a user searching by cpf.
+
+  ## Examples
+
+    - the user agent has to have been initialized
+
+          iex> {:ok, _pid} =  Flightex.start_agents
+
+    - creating a user
+
+          iex> user_params = %{name: "Maiqui", email: "maiquitome@gmail.com", cpf: "011.123.012-26"}
+
+          iex> {:ok, user_uuid} = Flightex.create_user(user_params)
+          {:ok, "b77f161c-13e7-4b61-9bcf-db87587bb179"}
+
+    - searching the user by cpf
+
+          iex> {:ok, user_found} = Flightex.Users.Agent.get_by_cpf("011.123.012-26")
+          {:ok,
+          %Flightex.Users.User{
+            cpf: "011.123.012-26",
+            email: "maiquitome@gmail.com",
+            id: "801737b0-6c1f-43b7-af51-97de2e574b96",
+            name: "Maiqui"
+          }}
+
+  """
+  def get_by_cpf(cpf) do
+    Agent.get(__MODULE__, fn state -> get_user_by_cpf(state, cpf) end)
   end
 
   @doc """
@@ -102,10 +171,19 @@ defmodule Flightex.Users.Agent do
     Map.put(state, cpf, user)
   end
 
-  defp get_user(state, cpf) do
-    case Map.get(state, cpf) do
-      nil -> {:error, "User not found!"}
-      user -> {:ok, user}
-    end
+  defp get_user_by_id(state, id) do
+    state
+    |> Map.values()
+    |> Enum.find(fn curr_user_map -> curr_user_map.id == id end)
+    |> handle_get_user()
   end
+
+  defp get_user_by_cpf(state, cpf) do
+    state
+    |> Map.get(cpf)
+    |> handle_get_user()
+  end
+
+  defp handle_get_user(%User{} = user), do: {:ok, user}
+  defp handle_get_user(nil), do: {:error, "User not found!"}
 end
