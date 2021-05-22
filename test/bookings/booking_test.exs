d05bc67daeef311a84a54e419537f77408df8965
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-05-22 17:02:23 -0300

    :white_check_mark: update tests

--- test/bookings/booking_test.exs
+++ test/bookings/booking_test.exs
@@ -1,25 +1,40 @@
 defmodule Flightex.Bookings.BookingTest do
+  @moduledoc false
+
   use ExUnit.Case, async: false
 
   alias Flightex.Bookings.Booking
 
   describe "build/4" do
-    test "when all params are valid, returns a booking" do
-      {_ok, response} =
+    setup do
+      Flightex.start_agents()
+
+      user_params = %{name: "Maiqui", email: "maiquitome@gmail.com", cpf: "011.123.012-26"}
+
+      {:ok, user_uuid} = Flightex.create_user(user_params)
+
+      {:ok, user_uuid: user_uuid}
+    end
+
+    test "when all params are valid, returns a booking", %{user_uuid: user_uuid} do
+      {:ok, %Booking{id: booking_id, complete_date: naive_date_time}} =
+        response =
         Booking.build(
-          ~N[2001-05-07 01:46:20],
+          "07/05/2001",
           "Brasilia",
           "ilha das bananas",
-          "12345678900"
+          user_uuid
         )
 
-      expected_response = %Flightex.Bookings.Booking{
-        complete_date: ~N[2001-05-07 01:46:20],
-        id: response.id,
-        local_destination: "ilha das bananas",
-        local_origin: "Brasilia",
-        user_id: "12345678900"
-      }
+      expected_response =
+        {:ok,
+         %Booking{
+           complete_date: naive_date_time,
+           id: booking_id,
+           local_destination: "ilha das bananas",
+           local_origin: "Brasilia",
+           user_id: user_uuid
+         }}
 
       assert response == expected_response
     end
