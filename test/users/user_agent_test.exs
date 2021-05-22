ddbaf06dc99738e75ea546a718d80af5615d1684
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-05-22 17:03:59 -0300

    :white_check_mark: add and update tests

--- test/users/user_agent_test.exs
+++ test/users/user_agent_test.exs
@@ -1,13 +1,16 @@
 defmodule Flightex.Users.AgentTest do
+  @moduledoc false
+
   use ExUnit.Case
 
   import Flightex.Factory
 
   alias Flightex.Users.Agent, as: UserAgent
+  alias Flightex.Users.User
 
   describe "save/1" do
     setup do
-      UserAgent.start_link(%{})
+      UserAgent.start_link()
 
       id = UUID.uuid4()
       cpf = "12345678900"
@@ -15,22 +18,56 @@ defmodule Flightex.Users.AgentTest do
       {:ok, id: id, cpf: cpf}
     end
 
-    test "when the user is saves, returns an tuple", %{id: id, cpf: cpf} do
-      :users
-      |> build(id: id, cpf: cpf)
-      |> UserAgent.save()
+    test "when the user is saved, returns a tuple", %{id: id, cpf: cpf} do
+      {:ok, _uuid} =
+        :user
+        |> build(id: id, cpf: cpf)
+        |> UserAgent.save()
 
-      response = UserAgent.get(cpf)
+      response = UserAgent.get_by_cpf(cpf)
 
-
-      expected_response =
-        {:ok, %Flightex.Users.User{cpf: cpf, email: "jp@banana.com", id: id, name: "Jp"}}
+      expected_response = {:ok, %User{cpf: cpf, email: "jp@banana.com", id: id, name: "Jp"}}
 
       assert response == expected_response
     end
   end
 
-  describe "get/1" do
+  describe "get_all/0" do
+    test "when the user is saved, returns a tuple" do
+      UserAgent.start_link()
+
+      {:ok, user_id1} =
+        :user
+        |> build(id: UUID.uuid4(), cpf: "12345678900")
+        |> UserAgent.save()
+
+      {:ok, user_id2} =
+        :user
+        |> build(name: "Maiqui", email: "maiqui@gmail.com", id: UUID.uuid4(), cpf: "12345678901")
+        |> UserAgent.save()
+
+      response = UserAgent.get_all()
+
+      expected_response = %{
+        "12345678900" => %User{
+          cpf: "12345678900",
+          email: "jp@banana.com",
+          id: user_id1,
+          name: "Jp"
+        },
+        "12345678901" => %User{
+          cpf: "12345678901",
+          email: "maiqui@gmail.com",
+          id: user_id2,
+          name: "Maiqui"
+        }
+      }
+
+      assert response == expected_response
+    end
+  end
+
+  describe "get_by_cpf/1" do
     setup do
       UserAgent.start_link(%{})
 
@@ -41,26 +78,27 @@ defmodule Flightex.Users.AgentTest do
     end
 
     test "when the user is found, returns the user", %{id: id, cpf: cpf} do
-      :users
-      |> build(id: id, cpf: cpf)
-      |> UserAgent.save()
+      {:ok, _uuid} =
+        :user
+        |> build(id: id, cpf: cpf)
+        |> UserAgent.save()
 
-      response = UserAgent.get(cpf)
+      response = UserAgent.get_by_cpf(cpf)
 
-      expected_response =
-        {:ok, %Flightex.Users.User{cpf: cpf, email: "jp@banana.com", id: id, name: "Jp"}}
+      expected_response = {:ok, %User{cpf: cpf, email: "jp@banana.com", id: id, name: "Jp"}}
 
       assert response == expected_response
     end
 
-    test "when the user is't founded, returns an error", %{id: id, cpf: cpf} do
-      :users
-      |> build(id: id, cpf: cpf)
-      |> UserAgent.save()
+    test "when the user is not found, returns an error", %{id: id, cpf: cpf} do
+      {:ok, _uuid} =
+        :user
+        |> build(id: id, cpf: cpf)
+        |> UserAgent.save()
 
-      response = UserAgent.get("banana")
+      response = UserAgent.get_by_cpf("banana")
 
-      expected_response = {:error, "User not found"}
+      expected_response = {:error, "User not found!"}
 
       assert response == expected_response
     end
