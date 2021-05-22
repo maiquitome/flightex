3224c358afa506d8fbd0575ceed089b9eb34ecc6
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-05-22 17:01:01 -0300

    :white_check_mark: add and update tests

--- test/bookings/booking_agent_test.exs
+++ test/bookings/booking_agent_test.exs
@@ -1,62 +1,119 @@
 defmodule Flightex.Bookings.AgentTest do
+  @moduledoc false
+
   use ExUnit.Case
 
   import Flightex.Factory
 
   alias Flightex.Bookings.Agent, as: BookingsAgent
+  alias Flightex.Bookings.Booking
+  alias Flightex.Users.Agent, as: UsersAgent
 
   describe "save/1" do
     setup do
-      BookingsAgent.start_link(%{})
+      BookingsAgent.start_link()
+      UsersAgent.start_link()
 
-      :ok
+      user_params = %{name: "Maiqui", email: "maiquitome@gmail.com", cpf: "011.123.012-26"}
+
+      {:ok, user_uuid} = Flightex.create_user(user_params)
+
+      {:ok, user_id: user_uuid}
     end
 
-    test "when the param are valid, return a booking uuid" do
+    test "when all params are valid, returns a booking uuid", %{user_id: user_uuid} do
       response =
         :booking
-        |> build()
+        |> build(user_id: user_uuid)
         |> BookingsAgent.save()
 
-      {_ok, uuid} = response
-
-      assert response == {:ok, uuid}
+      assert {:ok, _booking_id} = response
     end
   end
 
   describe "get/1" do
     setup do
-      BookingsAgent.start_link(%{})
+      BookingsAgent.start_link()
+      UsersAgent.start_link()
 
-      {:ok, id: UUID.uuid4()}
+      user_params = %{name: "Maiqui", email: "maiquitome@gmail.com", cpf: "011.123.012-26"}
+
+      {:ok, user_uuid} = Flightex.create_user(user_params)
+
+      {:ok, user_id: user_uuid}
     end
 
-    test "when the user is found, return a booking", %{id: id} do
-      booking = build(:booking, id: id)
-      {:ok, uuid} = BookingsAgent.save(booking)
+    test "when the booking id exists, returns the booking", %{user_id: user_uuid} do
+      {:ok, booking_id} =
+        :booking
+        |> build(user_id: user_uuid)
+        |> BookingsAgent.save()
 
-      response = BookingsAgent.get(uuid)
+      response = BookingsAgent.get(booking_id)
 
       expected_response =
         {:ok,
-         %Flightex.Bookings.Booking{
+         %Booking{
            complete_date: ~N[2001-05-07 03:05:00],
-           id: id,
+           id: booking_id,
            local_destination: "Bananeiras",
            local_origin: "Brasilia",
-           user_id: "12345678900"
+           user_id: user_uuid
          }}
 
       assert response == expected_response
     end
 
-    test "when the user wasn't found, returns an error", %{id: id} do
-      booking = build(:booking, id: id)
-      {:ok, _uuid} = BookingsAgent.save(booking)
-
+    test "when the booking wasn't found, returns an error" do
       response = BookingsAgent.get("banana")
 
-      expected_response = {:error, "Booking not found"}
+      expected_response = {:error, "Flight Booking not found!"}
+
+      assert response == expected_response
+    end
+  end
+
+  describe "get_all/0" do
+    setup do
+      BookingsAgent.start_link()
+      UsersAgent.start_link()
+
+      user_params = %{name: "Maiqui", email: "maiquitome@gmail.com", cpf: "011.123.012-26"}
+
+      {:ok, user_uuid} = Flightex.create_user(user_params)
+
+      {:ok, user_id: user_uuid}
+    end
+
+    test "returns all registered bookings", %{user_id: user_uuid} do
+      {:ok, booking_id1} =
+        :booking
+        |> build(user_id: user_uuid)
+        |> BookingsAgent.save()
+
+      {:ok, booking_id2} =
+        :booking
+        |> build(user_id: user_uuid, local_origin: "Porto Alegre")
+        |> BookingsAgent.save()
+
+      response = BookingsAgent.get_all()
+
+      expected_response = %{
+        booking_id1 => %Booking{
+          complete_date: ~N[2001-05-07 03:05:00],
+          id: booking_id1,
+          local_destination: "Bananeiras",
+          local_origin: "Brasilia",
+          user_id: user_uuid
+        },
+        booking_id2 => %Booking{
+          complete_date: ~N[2001-05-07 03:05:00],
+          id: booking_id2,
+          local_destination: "Bananeiras",
+          local_origin: "Porto Alegre",
+          user_id: user_uuid
+        }
+      }
 
       assert response == expected_response
     end
