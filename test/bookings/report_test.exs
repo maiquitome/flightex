be252132ef287b815c50e574297a9a1c803e87cd
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-05-22 17:03:13 -0300

    :white_check_mark: update tests

--- test/bookings/report_test.exs
+++ test/bookings/report_test.exs
@@ -1,33 +1,54 @@
 # Este teste é opcional, mas vale a pena tentar e se desafiar 😉
 
 defmodule Flightex.Bookings.ReportTest do
+  @moduledoc false
+
   use ExUnit.Case, async: true
 
   alias Flightex.Bookings.Report
 
-  describe "generate/1" do
+  describe "build/3" do
     setup do
       Flightex.start_agents()
 
-      :ok
+      user_params = %{name: "Maiqui", email: "maiquitome@gmail.com", cpf: "011.123.012-26"}
+
+      {:ok, user_uuid} = Flightex.create_user(user_params)
+
+      {:ok, user_id: user_uuid}
     end
 
-    test "when called, return the content" do
-      params = %{
-        complete_date: ~N[2001-05-07 12:00:00],
+    test "when called, returns the content", %{user_id: user_uuid} do
+      params1 = %{
+        complete_date: "07/05/2001",
         local_origin: "Brasilia",
-        local_destination: "Bananeiras",
-        user_id: "12345678900",
-        id: UUID.uuid4()
+        local_destination: "Bananeiras"
       }
 
-      content = "12345678900, Brasilia, Bananeiras,2001, 5,7,12,0,0"
+      {:ok, booking_id1} = Flightex.create_booking(user_uuid, params1)
 
-      Flightex.create_or_update_booking(params)
-      Report.generate("report-test.csv")
-      {_ok, file} = File.read("report-test.csv")
+      {:ok, %{complete_date: naive_date_time1}} = Flightex.get_booking(booking_id1)
 
-      assert file =~ content
+      content1 = "#{user_uuid},Brasilia,Bananeiras,#{naive_date_time1}\n"
+
+      params2 = %{
+        complete_date: "30/05/2001",
+        local_origin: "Brasilia",
+        local_destination: "Bananeiras"
+      }
+
+      {:ok, booking_id2} = Flightex.create_booking(user_uuid, params2)
+
+      {:ok, %{complete_date: naive_date_time2}} = Flightex.get_booking(booking_id2)
+
+      content2 = "#{user_uuid},Brasilia,Bananeiras,#{naive_date_time2}\n"
+
+      Report.build("06/05/2001", "30/05/2001", "report_test.csv")
+
+      {:ok, file} = File.read("report_test.csv")
+
+      assert file =~ content1
+      assert file =~ content2
     end
   end
 end
